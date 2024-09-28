435912d86a9eb6193622d755fa3ccb167364b08f
Sarah Robichaud <robichaudsarah@outlook.com>
2024-09-28 14:41:40 -0300

    wip - serve static project files

--- dockview-service/src/controllers/vaultController.ts
+++ dockview-service/src/controllers/vaultController.ts
@@ -1,9 +1,15 @@
+import { RequestHandler } from "express";
+import express from "express";
 import semver from "semver";
 import { vaultReader } from "~/server";
-import { TypedRequestHandler, V1Response } from "~/types/response";
 import {
-  ExtractVaultReaderResult,
-  ReadProjectVersionsOptions,
+	TypedRequestHandler,
+	V1ContentRequestResponse,
+	V1Response,
+} from "~/types/response";
+import {
+	ExtractVaultReaderResult,
+	ReadProjectVersionsOptions,
 } from "~/utils/local-vault";
 
 // Handler & Public API Response
@@ -14,29 +20,29 @@ type GetAllProjectsHandler = TypedRequestHandler<AllProjects>;
 export type GetAllProjectsResponse = V1Response<AllProjects>;
 
 export const getAllProjects: GetAllProjectsHandler = async (_req, res) => {
-  const [data, err] = vaultReader.readAllProjects();
+	const [data, err] = vaultReader.readAllProjects();
 
-  if (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-      type: err.name,
-      code: err.code,
-      data: null,
-    });
+	if (err) {
+		res.status(500).json({
+			success: false,
+			message: err.message,
+			type: err.name,
+			code: err.code,
+			resource: null,
+		});
 
-    return;
-  }
+		return;
+	}
 
-  res.status(200).json({
-    success: true,
-    resource: data,
-  });
+	res.status(200).json({
+		success: true,
+		resource: data,
+	});
 };
 
 // Handler
 type ProjectVersions = ExtractVaultReaderResult<
-  typeof vaultReader.readProjectVersions
+	typeof vaultReader.readProjectVersions
 >;
 type GetProjectVersionsHandler = TypedRequestHandler<ProjectVersions>;
 
@@ -47,56 +53,90 @@ export type GetProjectVersionsResponse = V1Response<ProjectVersions>;
  * Retrieves available versions of a project.
  */
 export const getProjectVersions: GetProjectVersionsHandler = async (
-  req,
-  res
+	req,
+	res
 ) => {
-  const projectName = req.params.version;
-  const { min, max } = req.query;
+	const projectName = req.params.projectName;
+	const { min, max } = req.query;
 
-  let options = { minimum: min, maximum: max } as ReadProjectVersionsOptions;
+	let options = { minimum: min, maximum: max } as ReadProjectVersionsOptions;
 
-  if ((min && !semver.valid(min)) || (max && !semver.valid(max))) {
-    res.status(400).json({
-      success: false,
-      code: 404,
-      message: "Invalid version provided (must be semver).",
-      type: "BadRequest",
-      data: null,
-    });
-    return;
-  }
+	if ((min && !semver.valid(min)) || (max && !semver.valid(max))) {
+		res.status(400).json({
+			success: false,
+			code: 404,
+			message: "Invalid version provided (must be semver).",
+			type: "BadRequest",
+			resource: null,
+		});
+		return;
+	}
 
-  // Shouldn't happen, should be infront of a parent route.
-  if (!projectName) {
-    res.status(400).json({
-      success: false,
-      code: 404,
-      message: "Project version not provided",
-      type: "BadRequest",
-      data: null,
-    });
+	// Shouldn't happen, should be infront of a parent route.
+	if (!projectName) {
+		res.status(400).json({
+			success: false,
+			code: 404,
+			message: "Project version not provided",
+			type: "BadRequest",
+			resource: null,
+		});
 
-    return;
-  }
+		return;
+	}
 
-  const [data, err] = vaultReader.readProjectVersions(projectName, options);
+	const [data, err] = vaultReader.readProjectVersions(projectName, options);
 
-  if (err) {
-    res.status(err.code);
+	if (err) {
+		res.status(err.code);
 
-    res.json({
-      success: false,
-      message: err.message,
-      type: err.name,
-      code: err.code,
-      data: null,
-    });
+		res.json({
+			success: false,
+			message: err.message,
+			type: err.name,
+			code: err.code,
+			resource: null,
+		});
 
-    return;
-  }
+		return;
+	}
 
-  res.status(200).json({
-    success: true,
-    resource: data,
-  });
+	res.status(200).json({
+		success: true,
+		resource: data,
+	});
+};
+
+// Public API Export
+export type ServeStaticAssetsResponse = V1ContentRequestResponse;
+
+export const serveStaticAssets: RequestHandler = (req, res, next) => {
+	const { projectName, version } = req.params;
+
+	if (!projectName || !version) {
+		res.status(400).json({
+			success: false,
+			code: 400,
+			message: "Project or version not provided",
+			type: "BadRequest",
+			resource: null,
+		});
+
+		return;
+	}
+
+	const [data, err] = vaultReader.getProjectAssetPath(projectName, version);
+
+	if (err) {
+		res.status(err.code).json({
+			success: false,
+			message: err.message,
+			type: err.name,
+			code: err.code,
+			resource: null,
+		});
+		return;
+	}
+
+	express.static(data.result)(req, res, next);
 };
--- dockview-service/src/routes/v1/vault.ts
+++ dockview-service/src/routes/v1/vault.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import {
-  getAllProjects,
-  getProjectVersions,
+	getAllProjects,
+	getProjectVersions,
+	serveStaticAssets,
 } from "@controllers/vaultController";
 
 const router = express.Router();
 
-router.get("/:version", getProjectVersions);
+router.get("/:projectName/:version/live", serveStaticAssets);
+
+router.get("/:projectName/:version", (req, res) => {
+	// Not implemented
+	res.status(501).send("Not implemented");
+});
+
+router.get("/:projectName", getProjectVersions);
 router.get("/", getAllProjects);
 
 export default router;
