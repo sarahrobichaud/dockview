4ce07c9804497798ae8b86d64e62af1b65c88762
Sarah Robichaud <robichaudsarah@outlook.com>
2025-01-28 13:07:35 -0400

    refactor: read all projects from source dir

--- apps/backend/src/controllers/vaultController.ts
+++ apps/backend/src/controllers/vaultController.ts
@@ -1,20 +1,21 @@
 /// <reference path="../types/lib/express.d.ts" />
 
-import { NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import semver from "semver";
 import { containerManager, vaultReader } from "~/server";
-import {
-	TypedRequestHandler,
-} from "~/types/response";
+import { TypedRequestHandler } from "~/types/response";
 
 import { V1Response } from "@dockview/core/api/types/responses";
 
 import {
-	ExtractVaultReaderResult,
-	ReadProjectVersionsOptions,
+  ExtractVaultReaderResult,
+  ReadProjectVersionsOptions,
 } from "~/utils/local-vault";
 
-import { DockviewServerContainer, DockviewStaticContainer } from "~/models/Container";
+import {
+  DockviewServerContainer,
+  DockviewStaticContainer,
+} from "~/models/Container";
 import { setupNginxEnvironment } from "~/pipelines/nginx.pipeline";
 import { fakeContainerStart } from "~/utils/fakeDelay";
 import { ContainerStatus } from "~/types/containerStatus.enum";
@@ -24,29 +25,30 @@ type AllProjects = ExtractVaultReaderResult<typeof vaultReader.readAllProjects>;
 type GetAllProjectsHandler = TypedRequestHandler<AllProjects>;
 
 export const getAllProjects: GetAllProjectsHandler = async (_req, res) => {
-	const [data, err] = vaultReader.readAllProjects();
+  const [data, err] = vaultReader.readAllProjects();
+  console.log({ data, err });
 
-	if (err) {
-		res.status(500).json({
-			success: false,
-			message: err.message,
-			type: err.name,
-			code: err.code,
-			resource: null,
-		});
+  if (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      type: err.name,
+      code: err.code,
+      resource: null,
+    });
 
-		return;
-	}
+    return;
+  }
 
-	res.status(200).json({
-		success: true,
-		resource: data,
-	});
+  res.status(200).json({
+    success: true,
+    resource: data,
+  });
 };
 
 // Handler
 type ProjectVersions = ExtractVaultReaderResult<
-	typeof vaultReader.readProjectVersions
+  typeof vaultReader.readProjectVersions
 >;
 type GetProjectVersionsHandler = TypedRequestHandler<ProjectVersions>;
 
@@ -57,218 +59,219 @@ export type GetProjectVersionsResponse = V1Response<ProjectVersions>;
  * Retrieves available versions of a project.
  */
 export const getProjectVersions: GetProjectVersionsHandler = async (
-	req,
-	res
+  req,
+  res
 ) => {
-	const projectName = req.params.projectName;
-	const { min, max } = req.query;
+  const projectName = req.params.projectName;
+  const { min, max } = req.query;
 
-	let options = { minimum: min, maximum: max } as ReadProjectVersionsOptions;
+  let options = { minimum: min, maximum: max } as ReadProjectVersionsOptions;
 
-	if ((min && !semver.valid(min)) || (max && !semver.valid(max))) {
-		res.status(400).json({
-			success: false,
-			code: 404,
-			message: "Invalid version provided (must be semver).",
-			type: "BadRequest",
-			resource: null,
-		});
-		return;
-	}
+  if ((min && !semver.valid(min)) || (max && !semver.valid(max))) {
+    res.status(400).json({
+      success: false,
+      code: 404,
+      message: "Invalid version provided (must be semver).",
+      type: "BadRequest",
+      resource: null,
+    });
+    return;
+  }
 
-	// Shouldn't happen, should be infront of a parent route.
-	if (!projectName) {
-		res.status(400).json({
-			success: false,
-			code: 404,
-			message: "Project version not provided",
-			type: "BadRequest",
-			resource: null,
-		});
+  // Shouldn't happen, should be infront of a parent route.
+  if (!projectName) {
+    res.status(400).json({
+      success: false,
+      code: 404,
+      message: "Project version not provided",
+      type: "BadRequest",
+      resource: null,
+    });
 
-		return;
-	}
+    return;
+  }
 
-	const [data, err] = vaultReader.readProjectVersions(projectName, options);
+  const [data, err] = vaultReader.readProjectVersions(projectName, options);
 
-	if (err) {
-		res.status(err.code);
+  if (err) {
+    res.status(err.code);
 
-		res.json({
-			success: false,
-			message: err.message,
-			type: err.name,
-			code: err.code,
-			resource: null,
-		});
+    res.json({
+      success: false,
+      message: err.message,
+      type: err.name,
+      code: err.code,
+      resource: null,
+    });
 
-		return;
-	}
+    return;
+  }
 
-	res.status(200).json({
-		success: true,
-		resource: data,
-	});
+  res.status(200).json({
+    success: true,
+    resource: data,
+  });
 };
 
 type ContainerRequestResult = {
-	containerURL: string;
-	cold: boolean;
-	statusURL: string;
+  containerURL: string;
+  cold: boolean;
+  statusURL: string;
 };
 type RequestContainerHandler = TypedRequestHandler<ContainerRequestResult>;
 
 // Public API Export
 export type RequestContainerResponse = V1Response<ContainerRequestResult>;
 
-export const requestContainer: RequestContainerHandler = async (req: Request, res: Response, next: NextFunction) => {
-	const { projectName, version } = req.params;
-	console.log("requestContainer", {projectName, version});
+export const requestContainer: RequestContainerHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { projectName, version } = req.params;
+  console.log("requestContainer", { projectName, version });
 
-	if (!projectName || !version) {
-		res.status(400).json({
-			success: false,
-			code: 400,
-			message: "Project or version not provided",
-			type: "BadRequest",
-			resource: null,
-		});
+  if (!projectName || !version) {
+    res.status(400).json({
+      success: false,
+      code: 400,
+      message: "Project or version not provided",
+      type: "BadRequest",
+      resource: null,
+    });
 
-		return;
-	}
+    return;
+  }
 
-	const [data, err] = vaultReader.getProjectAssetPath(projectName, version);
+  const [data, err] = await vaultReader.getProjectAssetPath(
+    projectName,
+    version
+  );
 
-	if (err) {
-		res.status(err.code).json({
-			success: false,
-			message: err.message,
-			type: err.name,
-			code: err.code,
-			resource: null,
-		});
-		return;
-	}
+  if (err) {
+    res.status(err.code).json({
+      success: false,
+      message: err.message,
+      type: err.name,
+      code: err.code,
+      resource: null,
+    });
+    return;
+  }
 
-	// TODO: This temporary, need to handle server containers
+  // TODO: This temporary, need to handle server containers
 
-	const availableInstance = containerManager.getExistingInstance(
-		projectName,
-		version
-	);
+  const availableInstance = containerManager.getExistingInstance(
+    projectName,
+    version
+  );
 
-	const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
-	const prefix = "dv--";
-	const baseDomain = `${process.env.DOMAIN || "localhost"}${
-		process.env.NODE_ENV === "production" ? "" : `:${process.env.PORT}`
-	}`;
+  const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+  const prefix = "dv--";
+  const baseDomain = `${process.env.DOMAIN || "localhost"}${
+    process.env.NODE_ENV === "production" ? "" : `:${process.env.PORT}`
+  }`;
 
-	// If an instance is available, escape and return the URL
-	if (availableInstance) {
-		res.json({
-			success: true,
-			resource: {
-				cold: !availableInstance.isReady,
-				containerURL: `${protocol}://${prefix}${availableInstance.id}.${baseDomain}`,
-				statusURL: `${protocol}://health.${baseDomain}/${availableInstance.id}`,
-			},
-		});
-		return;
-	}
+  // If an instance is available, escape and return the URL
+  if (availableInstance) {
+    res.json({
+      success: true,
+      resource: {
+        cold: !availableInstance.isReady,
+        containerURL: `${protocol}://${prefix}${availableInstance.id}.${baseDomain}`,
+        statusURL: `${protocol}://health.${baseDomain}/${availableInstance.id}`,
+      },
+    });
+    return;
+  }
 
+  // Handle static-server environment
+  if (req.containerRequest.settings.provided.environment === "static-server") {
+    const serverContainer = new DockviewServerContainer(projectName, version);
 
-	// Handle static-server environment
-	if(req.containerRequest.settings.provided.environment === "static-server") {
+    containerManager.registerContainer(serverContainer);
 
-		const serverContainer = new DockviewServerContainer(
-			projectName,
-			version
-		);
-		
-		containerManager.registerContainer(serverContainer);
+    fakeContainerStart(1000).then(async () => {
+      const container = await setupNginxEnvironment(
+        serverContainer.id,
+        req.containerRequest.project + "-" + req.containerRequest.version,
+        {
+          sourceDir: req.containerRequest.sourcePath,
+          buildDir: req.containerRequest.buildPath,
+        },
+        req.selectedMode
+      );
 
-		fakeContainerStart(1000).then(async () => {
-			const container = await setupNginxEnvironment(
-				serverContainer.id,
-				req.containerRequest.project + "-" + req.containerRequest.version,
-				{
-				sourceDir: req.containerRequest.sourcePath,
-				buildDir:req.containerRequest.buildPath
-			},
-			req.selectedMode,
-		);
+      setTimeout(() => {
+        serverContainer.status = ContainerStatus.TRANSITION;
+        serverContainer.attach(container);
+      }, 8000);
+    });
 
-			setTimeout(() => {
-				serverContainer.status = ContainerStatus.TRANSITION;
-				serverContainer.attach(container);
-			}, 8000);
-		});
+    const containerID = serverContainer.id;
+    const target = `${protocol}://${prefix}${containerID}.${baseDomain}`;
+    const statusURL = `${protocol}://health.${baseDomain}/${containerID}`;
 
-		const containerID = serverContainer.id;
-		const target = `${protocol}://${prefix}${containerID}.${baseDomain}`;
-		const statusURL = `${protocol}://health.${baseDomain}/${containerID}`;
+    return res.json({
+      success: true,
+      resource: {
+        cold: true,
+        containerURL: target,
+        statusURL: statusURL,
+      },
+    });
+  }
 
-		return res.json({
-			success: true,
-			resource: {
-				cold: true,
-				containerURL: target,
-				statusURL: statusURL,
-			},
-		});
+  const containerInstance = new DockviewStaticContainer(
+    data.result,
+    projectName,
+    version
+  );
 
-	}
+  containerManager.registerContainer(containerInstance);
 
-	const containerInstance = new DockviewStaticContainer(
-		data.result,
-		projectName,
-		version
-	);
+  const containerID = containerInstance.id;
+  const target = `${protocol}://${prefix}${containerID}.${baseDomain}`;
 
-	containerManager.registerContainer(containerInstance);
+  fakeContainerStart(1000).then(() => {
+    const container = containerManager.getContainer(containerID);
+    if (container) {
+      container.status = ContainerStatus.LAUNCHING;
+    }
+  });
+  fakeContainerStart(2000).then(() => {
+    const container = containerManager.getContainer(containerID);
+    if (container) {
+      container.status = ContainerStatus.BUILD_IMAGE;
+    }
+  });
+  fakeContainerStart(3000).then(() => {
+    const container = containerManager.getContainer(containerID);
+    if (container) {
+      container.status = ContainerStatus.SPIN_UP;
+    }
+  });
+  fakeContainerStart(4000).then(() => {
+    const container = containerManager.getContainer(containerID);
+    if (container) {
+      container.status = ContainerStatus.READY;
+    }
+  });
 
-	const containerID = containerInstance.id;
-	const target = `${protocol}://${prefix}${containerID}.${baseDomain}`;
+  fakeContainerStart(4700).then(() => {
+    const container = containerManager.getContainer(containerID);
+    if (container) {
+      container.status = ContainerStatus.TRANSITION;
+    }
+  });
 
-	fakeContainerStart(1000).then(() => {
-		const container = containerManager.getContainer(containerID);
-		if (container) {
-			container.status = ContainerStatus.LAUNCHING;
-		}
-	});
-	fakeContainerStart(2000).then(() => {
-		const container = containerManager.getContainer(containerID);
-		if (container) {
-			container.status = ContainerStatus.BUILD_IMAGE;
-		}
-	});
-	fakeContainerStart(3000).then(() => {
-		const container = containerManager.getContainer(containerID);
-		if (container) {
-			container.status = ContainerStatus.SPIN_UP;
-		}
-	});
-	fakeContainerStart(4000).then(() => {
-		const container = containerManager.getContainer(containerID);
-		if (container) {
-			container.status = ContainerStatus.READY;
-		}
-	});
-
-	fakeContainerStart(4700).then(() => {
-		const container = containerManager.getContainer(containerID);
-		if (container) {
-			container.status = ContainerStatus.TRANSITION;
-		}
-	});
-
-	res.json({
-		success: true,
-		resource: {
-			cold: true,
-			containerURL: target,
-			statusURL: `${protocol}://health.${baseDomain}/${containerID}`,
-		},
-	});
-	// res.redirect(target);
+  res.json({
+    success: true,
+    resource: {
+      cold: true,
+      containerURL: target,
+      statusURL: `${protocol}://health.${baseDomain}/${containerID}`,
+    },
+  });
+  // res.redirect(target);
 };
--- apps/backend/src/proxy/projectProxy.ts
+++ apps/backend/src/proxy/projectProxy.ts
@@ -1,73 +1,72 @@
 import { Request, RequestHandler } from "express";
 import express from "express";
 import {
-	DockviewServerContainer,
-	DockviewStaticContainer,
+  DockviewServerContainer,
+  DockviewStaticContainer,
 } from "~/models/Container";
-import {  proxy } from "~/proxy";
+import { proxy } from "~/proxy";
 import { containerManager } from "~/server";
 import { ContainerStatus } from "~/types/containerStatus.enum";
 
-
 export const projectProxyHandler: RequestHandler = (req, res, next) => {
+  console.log("-------------- projectProxyHandler --------------");
 
-	console.log("-------------- projectProxyHandler --------------");
+  const subdomain = req.hostname.split(".")[0];
 
-	const subdomain = req.hostname.split(".")[0];
+  // Extract projectName, version, and containerID from the subdomain
+  // Assuming subdomain format: projectName--version--containerID
+  const [prefix, containerID] = subdomain.split("--");
 
-	// Extract projectName, version, and containerID from the subdomain
-	// Assuming subdomain format: projectName--version--containerID
-	const [prefix, containerID] = subdomain.split("--");
+  const secFetchSite = req.headers["sec-fetch-site"];
 
-	const secFetchSite = req.headers["sec-fetch-site"];
+  // Protect route
+  if (secFetchSite !== "same-origin") {
+    // Deny the request
+    console.log("Forbidden" + req.hostname);
+    res.redirect("/");
+    // res.status(403).send("Forbidden");
+  }
 
-	// Protect route
-	if (secFetchSite !== "same-origin") {
-		// Deny the request
-		console.log("Forbidden" + req.hostname);
-		res.redirect("/");
-		// res.status(403).send("Forbidden");
-	}
+  if (prefix !== "dv") {
+    res.status(400).send("Invalid subdomain.");
+  }
 
-	if (prefix !== "dv") {
-		res.status(400).send("Invalid subdomain.");
-	}
+  if (!containerID) {
+    res.status(400).send("Container ID not provided.");
+  }
 
-	if (!containerID) {
-		res.status(400).send("Container ID not provided.");
-	}
+  console.log("Container ID:", containerID);
 
-	console.log("Container ID:", containerID);
+  // Check if the containerID is valid
+  const container = containerManager.getContainer(containerID);
 
-	// Check if the containerID is valid
-	const container = containerManager.getContainer(containerID);
+  if (!container) {
+    return res.status(404).send("Container not found.");
+  }
 
-	if (!container) {
-		return res.status(404).send("Container not found.");
-	}
+  container.updateLastAccessed();
 
-	container.updateLastAccessed();
+  if (container instanceof DockviewServerContainer) {
+    // Not implemented yet
+    console.log("-------------- is server --------------");
 
-	if (container instanceof DockviewServerContainer) {
-		// Not implemented yet
-		console.log("-------------- is server --------------");
+    if (container.status !== ContainerStatus.TRANSITION) {
+      res.render("launching");
+      return;
+    }
 
-		if (container.status !== ContainerStatus.TRANSITION) {
-			res.render("launching");
-			return;
-		}
+    proxy.web(req, res, {
+      target: `http://${container.ip}:${container.port}`,
+      changeOrigin: true,
+      ws: false,
+    });
+    return;
+  }
 
-		proxy.web(req, res, {
-			target: `http://${container.ip}:${container.port}`,
-			changeOrigin: true,
-			ws: false,
-		});
-		return;
-	}
+  console.log("------------- is static ------------");
 
-	console.log('------------- is static ------------');
-
-	if (container instanceof DockviewStaticContainer) {
-		express.static(container.path)(req, res, next);
-	}
+  if (container instanceof DockviewStaticContainer) {
+    console.log({ path: container.path });
+    express.static(container.path)(req, res, next);
+  }
 };
