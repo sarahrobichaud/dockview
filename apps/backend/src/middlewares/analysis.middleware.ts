d91634cf4e9b1bd1088e88d94010ff64f034440f
Sarah Robichaud <robichaudsarah@outlook.com>
2025-03-07 14:49:04 -0400

    fix: throw custom error is project analyzer fails

--- apps/backend/src/middlewares/analysis.middleware.ts
+++ apps/backend/src/middlewares/analysis.middleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import {ProjectAnalyzer} from "~/lib/project-analyzer/ProjectAnalyzer";
+import { ProjectAnalyzer } from "~/lib/project-analyzer/ProjectAnalyzer";
 import { ProjectQuery } from "@dockview/core/shared";
 import { DockviewError } from "~/errors/DockviewError";
 import { container } from "tsyringe";
@@ -13,20 +13,24 @@ export const analyzeProject = async (req: Request, res: Response, next: NextFunc
     const projectAnalyzer = container.resolve<ProjectAnalyzer>(TOKENS.ProjectAnalyzer);
     const vaultService = container.resolve<VaultService>(TOKENS.VaultService);
 
-    const query: ProjectQuery = {name: req.params.projectName, version: req.params.version};
+    const query: ProjectQuery = { name: req.params.projectName, version: req.params.version };
 
     // TODO: Consistent error handling
-    if(!query.name || !query.version) {
+    if (!query.name || !query.version) {
         return next(new DockviewError("Project name and version are required", 400));
     }
 
     // Verify that the project version exists
-    if(!vaultService.hasProjectVersion(query)) {
+    if (!vaultService.hasProjectVersion(query)) {
         return next(new DockviewError("Project version not found", 404));
     }
 
     // Analyze the project
-    req.projectAnalysis = await projectAnalyzer.analyze(query);
+    try {
+        req.projectAnalysis = await projectAnalyzer.analyze(query);
+    } catch (error) {
+        return next(new DockviewError(`Project version ${query.version} not found`, 404));
+    }
 
     next();
 }
